ee4c69075c2e39196e136927c5856a272c57b9df
Chris Ferrer <imjunjun24@gmail.com>
2026-01-20 11:35:04 +0800

    modified:   app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -21,4 +21,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
